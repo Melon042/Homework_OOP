faa9d5383a52363ddc706590d0e0052d973da747
Dmitry <dmitry.babakov97@gmail.com>
2025-11-20 06:03:18 +0300

    14.2 done

--- src/classes.py
+++ src/classes.py
@@ -3,14 +3,55 @@ class Product:
 
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        if price <= 0:
+            raise ValueError("Цена не должна быть нулевая или отрицательная")
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_data: dict, old_products: list = None):
+        """Создаёт новый объект класса Product из словаря"""
+        if old_products is None:
+            old_products = []
+
+        name = product_data['name']
+        description = product_data['description']
+        price = product_data['price']
+        quantity = product_data['quantity']
+
+        new_product = cls(name, description, price, quantity)
+
+        for product in old_products:
+            if product.name == new_product.name:
+                new_product.quantity += product.quantity
+                if product.price > new_product.price:
+                    new_product.price = product.price
+
+        return new_product
+
+    @property
+    def price(self):
+        """Возвращает цену объекта Product"""
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        """Присваивает новую цену с проверкой корректности значения"""
+        if value <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+        else:
+            if value < self.__price:
+                answer = input('Новая цена ниже предыдущей, введите "y" для подтверждения или "n" для отмены.')
+                if answer == 'y':
+                    self.__price = value
+            else:
+                self.__price = value
 
 
 class Category:
@@ -18,7 +59,7 @@ class Category:
 
     name: str
     description: str
-    products: list
+    __products: list
 
     category_count = 0
     product_count = 0
@@ -26,6 +67,22 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    def add_product(self, product):
+        """Добавляет объект класса Product в список товаров категории"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self):
+        """Возвращает список строк товаров объекта Product"""
+        products = []
+        for product in self.__products:
+            products.append(f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.')
+
+        return products
